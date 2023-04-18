c9f38a909c038abf298971757c2ce79336e268c0
Olivia Allam <oliviaallam@Olivias-MacBook-Air.local>
2023-04-17 21:44:56 -0400

    changed buttons for student or instructor

--- final-project/src/views/HomeView.vue
+++ final-project/src/views/HomeView.vue
@@ -6,13 +6,14 @@
     import { UserType } from "./DataTypes.vue";
 
     var auth: Auth | null = null
-    // var my_uid: string = ""
+    var role = ("")
 
     const appNav = useRouter()
     const db:Firestore = getFirestore();
     const course_name = ref("")
     const course_code  = ref("")
     const u_fname = ref("")
+    
 
     type HomeViewDetailType = {
         userId: string;
@@ -20,21 +21,19 @@
 
     const props = defineProps<HomeViewDetailType>()
 
-        const myDoc:DocumentReference = doc(db, `users/${props.userId}`);
+    const myDoc:DocumentReference = doc(db, `users/${props.userId}`);
 
-        getDoc(myDoc).then(
-            (qd:DocumentSnapshot) => {
-                if (qd.exists()) {
-                    const userData = qd.data() as UserType
-                    u_fname.value = userData.fname;
-                }
+    getDoc(myDoc).then(
+        (qd:DocumentSnapshot) => {
+            if (qd.exists()) {
+                const userData = qd.data() as UserType
+                u_fname.value = userData.fname;
+                role = userData.role;
+            }
         })
 
     onMounted(() => {
         auth = getAuth();
-       /*auth!.onAuthStateChanged(user => {
-            if (user) { my_uid = user.uid; }
-        }) */
     })
 
     function goToNewCourse() {
@@ -54,12 +53,12 @@
     <div><h3 :key="u_fname">Welcome, {{ u_fname }}!<button class="signoutbutton" @click="logOut">Sign Out</button></h3></div>
     <div class="parent">
         <div class="div1">
-            <p>My Courses</p>
+            <p>My Courses 
+                <button v-if="role == 'Instructor'" @click="goToNewCourse">+ New Course</button>
+                <button v-else-if="role == 'Student'" @click="goToNewCourse">Join Course</button>
+            </p>
         </div>
         <div class="div2">
-        </div>
-        <div class="div3">
-            <button @click="goToNewCourse">+ New Course</button>
         </div>
     </div>
 </template>
@@ -85,28 +84,22 @@
     }
 
     .div1 { 
-        grid-area: 2 / 1 / 6 / 3; 
+        grid-area: 1 / 1 / 6 / 3;
         background-color: #3b3b3b;
         border-color: black;
         height: 100%;
         width: 100%;
     }
-    .div2 { grid-area: 2 / 3 / 6 / 6;
+    .div2 { grid-area: 1 / 3 / 6 / 6;
         background-color: #3b3b3b;
         border-color: black; 
         height: 100%;
         width: 100%;
     }
-    .div3 { grid-area: 1 / 1 / 2 / 6; 
-        background-color: #3b3b3b;
-        border-color: black;
-        height: 100%;
-        width: 100%;
-        text-align: center;
-    }
 
     button {
-        padding: 20px;
+        float: right;
+        margin-right: 20px;
     }
 
     .signoutbutton {
